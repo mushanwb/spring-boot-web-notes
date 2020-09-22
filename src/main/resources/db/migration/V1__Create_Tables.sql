5b00d210d1019f00b21f28bc9f9a4b32c656352a
mushan <49012328+mushanwb@users.noreply.github.com>
2020-09-22 11:22:13 +0800

    添加 数据表 (#6)
    添加 表结构，初始化数据表里的数据
    注意：V1 后面一定是两个 _ ，如果写成一个，则找不到数据库迁移文件。
    
    执行 mvn flyway:migrate 迁移数据
    执行 mvn flyway:clean  清除数据

---
+++ src/main/resources/db/migration/V1__Create_Tables.sql
@@ -0,0 +1,14 @@
+create table user (
+    id bigint primary key auto_increment,
+    name varchar (100)
+);
+
+create table match (
+    id bigint primary key auto_increment,
+    user_id bigint,
+    score int
+);
+
+insert into user (id, name) values (1, '张三'), (2, '李四');
+
+insert into match(id, user_id, score) values (1, 1, 1000), (2, 1, 2000), (3, 2, 500), (4, 2, 3000);
