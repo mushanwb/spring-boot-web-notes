88ae24bcbc7ef13029ad18d5d27849d8cbe45d79
wangbo <1216476406@qq.com>
2020-09-23 17:27:09 +0800

    添加 Redis
    在这个提交中，我添加了 redis
    在 pom.xml 引入 spring-boot 的 redis 依赖
    同时在 application.properties 文件中添加 redis 的配置信息
    最后创建一个 RedisConfig 配置

---
+++ src/main/java/com/mushanwb/github/config/RedisConfig.java
@@ -0,0 +1,18 @@
+package com.mushanwb.github.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    RedisTemplate<String, java.lang.Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        return redisTemplate;
+    }
+
+}
