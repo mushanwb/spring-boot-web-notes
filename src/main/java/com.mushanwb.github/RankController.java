e2a0d6a9e5c01bcc82e852602d15399f2dff9e74
wangbo <1216476406@qq.com>
2020-09-24 10:55:52 +0800

    添加 cache 注解
    使用 redis 创建了一个 注解，在接口数据输出的时候使用 cache 注解

--- src/main/java/com.mushanwb.github/RankController.java
+++ src/main/java/com.mushanwb.github/RankController.java
@@ -1,5 +1,6 @@
 package com.mushanwb.github;
 
+import com.mushanwb.github.anno.Cache;
 import com.mushanwb.github.service.RankService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class RankController {
      * 字段包括：姓名和分数
      * @return Object
      */
+    @Cache
     @RequestMapping("/rank")
     public Object rank() {
         return rankService.getRank();
